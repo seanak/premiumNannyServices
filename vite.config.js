55d108b05c30db239ba54ff369dca90aaaba919a
f8d1170b-615a-47f8-ab67-43cd478c9485 <MzIyOTM5MTAxMTcyOtPYW7Gvhn00XOcPkGQKhARWu3Wl>
2025-09-06 00:15:32 +0200

    Fix: Reconfigure for new repository premiumnanny_services

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/premiumnanny_website/',
+  base: '/premiumnanny_services/',
   plugins: [react()],
 })
